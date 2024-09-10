c65476e43d331284a1701705f3e52cc8e6c13075
WEBER JULIETA <49318078@ort>
2024-09-10 10:16:46 -0300

    listo

---
+++ index.js
@@ -0,0 +1,19 @@
+let hola = document.getElementById("demo");
+hola.innerHTML =  (" ")
+document.querySelector(".bebida").style.display = "none"
+let alfa = document.querySelectorAll(".alfajor");
+    
+for(i = 0; i< alfa.length;i++){
+    alfa[i].style.backgroundColor = "blue"
+}
+
+const jugadores = document.querySelectorAll('.ca.mundial');
+
+jugadores[0].style.backgroundColor = "gold"
+
+for(i = 0; i< jugadores.length;i++){
+    jugadores[i].style.backgroundColor = "gold"
+}
+
+let empanadas = document.getElementById("empanadaSiete")
+
